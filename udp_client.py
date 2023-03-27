37f69a118b8d4d890b5f68eabbcd1b63655ba3a7
Jonathan Rooney <jonathan.m.rooney@gmail.com>
2023-03-27 19:33:57 -0400

    Testing camera on UDP server - ROV side

--- udp_client.py
+++ udp_client.py
@@ -1,10 +1,34 @@
 import socket
+import time
+
+import picamera2
 
 UDP_IP = "10.0.0.2"
 UDP_PORT = 5000
-MESSAGE = b"Hello, world"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+img_x = 800
+img_y = 600
+bitrate = 10000000
 
-sock.close()
+cam = picamera2.Picamera2()
+cam.video_configuration.size = (img_x, img_y)
+cam.video_configuration.controls.FrameRate = 25.0
+cam.configure('video')
+
+encoder = picamera2.encoders.H264Encoder(bitrate=bitrate)
+cam.encoder = encoder
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((UDP_IP, UDP_PORT))
+    sock.listen()
+    
+    conn, addr = sock.accept()
+    stream = conn.makefile('wb')
+    cam.encoder.output = picamera2.outputs.FileOutput(stream)
+    cam.start_encoder()
+    cam.start()
+    time.sleep(15)
+    cam.stop()
+    cam.stop_encoder()
+    conn.close()
--- udp_server.py
+++ udp_server.py
@@ -1,19 +1,12 @@
 import socket
 import time
 
-UDP_IP = "10.0.0.1"
+UDP_IP = "10.0.0.2"
 UDP_PORT = 5000
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
-
-while True:
-    localtime = time.time_ns()
-    data, addr = sock.recvfrom(1024)
-    break
-    
-sock.close()
-
-sendtime = data.decode('utf-8')
-diff = (localtime - int(sendtime))/1e9
-print('Time difference: {}'.format(diff))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((UDP_IP, UDP_PORT))
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if not data:
+            break
