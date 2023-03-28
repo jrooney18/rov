b9ac3000ffbfc4f9099e962121e2bfac18db946e
Jonathan Rooney <jonathan.m.rooney@gmail.com>
2023-03-27 21:09:56 -0400

    Discovered UDP server was using TCP. Attemping to rectify

--- udp_client.py
+++ udp_client.py
@@ -3,36 +3,75 @@ import time
 
 import picamera2
 
-rov_ip = "10.0.0.1"
-rov_port = 5000
+# rov_ip = "10.0.0.1"
+# rov_port = 5000
 
+# img_x = 800
+# img_y = 600
+# bitrate = 10000000
+
+# cam = picamera2.Picamera2()
+# cam.video_configuration.size = (img_x, img_y)
+# cam.video_configuration.controls.FrameRate = 25.0
+# cam.configure('video')
+
+# encoder = picamera2.encoders.H264Encoder()
+# cam.encoder = encoder
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+#     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#     sock.bind((rov_ip, rov_port))
+#     sock.listen()
+#     print('Waiting for connection...')
+    
+#     conn, addr = sock.accept()
+#     print('Connection established. Beginning stream...')
+#     stream = conn.makefile('wb')
+#     cam.encoder.output = picamera2.outputs.FileOutput(stream)
+#     cam.start_encoder()
+#     cam.start()
+#     time.sleep(15)
+#     cam.stop()
+#     cam.stop_encoder()
+#     print('Ending stream.')
+#     conn.close()
+# print('All connections closed. Shutting down ROV server.')
+
+
+import io
+
+BUFF_SIZE = 65536
 img_x = 800
 img_y = 600
-bitrate = 10000000
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+host_name = socket.gethostname()
+host_ip = '10.0.0.1'
+port = 5000
+socket_address = (host_ip,port)
+server_socket.bind(socket_address)
 
 cam = picamera2.Picamera2()
 cam.video_configuration.size = (img_x, img_y)
-cam.video_configuration.controls.FrameRate = 25.0
-cam.configure('video')
+cam.configure('still')
 
-encoder = picamera2.encoders.H264Encoder(bitrate=bitrate)
+encoder = picamera2.encoders.JpegEncoder()
 cam.encoder = encoder
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((rov_ip, rov_port))
-    sock.listen()
-    print('Waiting for connection...')
-    
-    conn, addr = sock.accept()
-    print('Connection established. Beginning stream...')
-    stream = conn.makefile('wb')
-    cam.encoder.output = picamera2.outputs.FileOutput(stream)
-    cam.start_encoder()
-    cam.start()
-    time.sleep(15)
-    cam.stop()
-    cam.stop_encoder()
-    print('Ending stream.')
-    conn.close()
-print('All connections closed. Shutting down ROV server.')
+cam.start_encoder()
+cam.start()
+
+print('Listening at:',socket_address)
+
+while True:
+    msg,client_addr = server_socket.recvfrom(BUFF_SIZE)
+    print('GOT connection from ',client_addr)
+    for frames in range(1, 250):
+        img_buffer = io.BytesIO()
+        cam.capture_file(img_buffer, format='jpeg')
+        server_socket.sendto(img_buffer, client_addr)
+        time.sleep(1)
+    break
+server_socket.close()
+print('ROV server shut down.')
--- udp_server.py
+++ udp_server.py
@@ -2,18 +2,34 @@ import socket
 import subprocess
 import time
 
-rov_ip = "10.0.0.1"
-rov_port = 5000
+# rov_ip = "10.0.0.1"
+# rov_port = 5000
 
-cmdline = [r'C:\Program Files\VideoLAN\VLC\vlc.exe', '--demux', 'h264', '-']
-player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
+# cmdline = [r'C:\Program Files\VideoLAN\VLC\vlc.exe', '--demux', 'jpg', '-']
+# player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((rov_ip, rov_port))
-    while True:
-        data, addr = sock.recvfrom(1024)
-        if not data:
-            break
-        player.stdin.write(data)
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+#     sock.connect((rov_ip, rov_port))
+#     while True:
+#         data, addr = sock.recvfrom(1024)
+#         if not data:
+#             break
+#         player.stdin.write(data)
 
-player.terminate()
+# player.terminate()
+
+
+BUFF_SIZE = 65536
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+client_socket.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
+host_name = socket.gethostname()
+host_ip = '10.0.0.1'
+port = 5000
+message = b'Hello'
+
+client_socket.sendto(message,(host_ip,port))
+while True:
+	packet,_ = client_socket.recvfrom(BUFF_SIZE)
+    if not packet:
+        client_socket.close()
+        break
